9a3df867a7d694285e5b7ec8bf9dda20d040566c
tokam107 <CDS.ToqaMuhammad23795@alexu.edu.eg>
2026-02-23 20:34:52 +0200

    Refactor test_graph.py: Add necessary imports for module path configuration

--- app/testings/test_graph.py
+++ app/testings/test_graph.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from app.graph.graph import build_graph
 
 graph = build_graph()
